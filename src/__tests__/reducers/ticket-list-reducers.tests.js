6f2513dfa02866259dc7ea54083d58cd0b4d6ef1
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-16 10:32:32 -0800

    Test for adding a ticket.

--- src/__tests__/reducers/ticket-list-reducers.tests.js
+++ src/__tests__/reducers/ticket-list-reducers.tests.js
@@ -1,7 +1,34 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 
 describe('ticketListReducer', () => {
+  let action;
+  const ticketData = {
+    names:'Ryan & Aimen',
+    location: '4B',
+    issue: 'Redux action is not working',
+    id: 2
+  };
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should succesfully addd new ticket data to theMainTickeList ',() => {
+    const {names, location, issue, id } = ticketData;
+    action = {
+      type: 'ADD_TICKET',
+      names = names,
+      location: location,
+      issue: issue,
+      id: id
+    };
+
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names:names,
+        location:location,
+        issue: issue,
+        id: id 
+      }
+    });
   });
 });
