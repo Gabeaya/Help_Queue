0a41b8ba238a5725ddf8a6f5e96644b8f58014a2
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-18 19:06:00 -0800

    Write a test that defines the action type of delete and id of i to expect it to return only updted objects.

--- src/__tests__/reducers/ticket-list-reducers.tests.js
+++ src/__tests__/reducers/ticket-list-reducers.tests.js
@@ -7,7 +7,20 @@ describe('ticketListReducer', () => {
     location: '4B',
     issue: 'Redux action is not working',
     id: 2
+    
   };
+  const currentState = {
+    1: {names: 'Ryan & Aimen',
+    location: '4b',
+    issue: 'Redux action is not working correctly.',
+    id: 1 },
+    2: {names: 'Jasmine and Justine',
+    location: '2a',
+    issue: 'Reducer has side effects.',
+    id: 2 }
+  }
+
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
@@ -31,4 +44,18 @@ describe('ticketListReducer', () => {
       }
     });
   });
+
+  test('Should successfully delete a ticket', () => { 
+    action = {
+      type: 'DELETE_TICKET',
+      id: 1
+    };
+    expect(ticketListReducer(currentState, action)).toEqual({
+      2: {names: 'Jasmine and Justine',
+        location: '2a',
+        issue: 'Reducer has side effects.',
+        id: 2 }
+    });
+  });
+
 });
