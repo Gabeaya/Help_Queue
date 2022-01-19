b7a1b364ab9a74a7409c9c44f3be7605bde2ed5e
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-18 20:20:23 -0800

    define the actionand dispatch for our editing functionality.

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -74,17 +74,22 @@ class TicketControl extends React.Component {
     this.setState({editing: true});
   }
 
-  handleEditingTicketInList = (ticketToEdit) => {
-    const editedMainTicketList = this.state.mainTicketList
-      .filter(ticket => ticket.id !== this.state.selectedTicket.id)//this removes the old ticket
-      .concat(ticketToEdit);//this adds the new one
-    this.setState({
-      mainTicketList: editedMainTicketList,//this updates our ticket list
-      editing: false,
-      selectedTicket: null
-    });
-
+handleEditingTicketInList = (ticketToEdit) => {
+  const { dispatch } = this.props;
+  const { id, names, location, issue } = ticketToEdit;
+  const action = {
+    type: 'ADD_TICKET',
+    id: id,
+    names: names,
+    location: location,
+    issue: issue,
   }
+  dispatch(action);
+  this.setState({
+    editing: false,
+    selectedTicket: null
+  });
+}
 
   render(){  
     let currentlyVisibleState = null;
