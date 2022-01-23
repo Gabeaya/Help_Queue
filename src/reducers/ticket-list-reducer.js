cf8ed63d5b03cc22080217ca28d42d61b9578a0a
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-23 14:16:17 -0800

    Add the action constant.

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,7 +1,8 @@
+import * as c from './../actions/ActionTypes';
 export default (state = {}, action) => {//we use default because there will only be one function below
   const { names, location, issue, id } = action;
   switch (action.type) {
-  case 'ADD_TICKET': //lines 4 through 11 are the switch case if the action is equal to add ticket
+  case c.ADD_TICKET: //lines 4 through 11 are the switch case if the action is equal to add ticket
     return Object.assign({}, state, {//we need the '{}' in order to creat a copy and not directly affect the state
       //the second argument is the object that will be cloned
       //the third is the change made to the copy
@@ -12,7 +13,7 @@ export default (state = {}, action) => {//we use default because there will only
         id: id
       }
     });
-  case 'DELETE_TICKET':
+  case c.DELETE_TICKET:
     let newState = { ...state };//makes a copy of the current state
     delete newState[id];//uses delete function to remove the key value pair
     return newState; //returns our updated state
