0f5fcb59d16dbbc8138b77fb258eaba3a25ed184
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-23 12:45:25 -0800

    Update add ticket action creator.

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,15 @@ export const deleteTicket = id => ({
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
-export const addTicket = () => ({
-  type:'ADD_TICKET'
-});
+export const addTicket = (ticket) => {
+  const { names, location, issue, id} = ticket;
+  return {
+    type:'ADD_TICKET',
+    names: names,
+    location: location,
+    issue: issue,
+    id: id
+  }
+}
+
+
