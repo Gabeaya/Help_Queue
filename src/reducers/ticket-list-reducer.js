8cb07c0b9144bc2c0cb15ff290126c16543789f7
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-29 13:19:24 -0800

    Add a reducer that updates the formatted wait time when  ticket is opened.

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,6 +1,6 @@
 import * as c from './../actions/ActionTypes';
 export default (state = {}, action) => {//we use default because there will only be one function below
-  const { names, location, issue, id } = action;
+  const { names, location, issue, id, formattedWaitTime, timeOpen } = action;
   switch (action.type) {
   case c.ADD_TICKET: //lines 4 through 11 are the switch case if the action is equal to add ticket
     return Object.assign({}, state, {//we need the '{}' in order to creat a copy and not directly affect the state
@@ -17,8 +17,13 @@ export default (state = {}, action) => {//we use default because there will only
     let newState = { ...state };//makes a copy of the current state
     delete newState[id];//uses delete function to remove the key value pair
     return newState; //returns our updated state
-    default:
-      return state;
+  case c.UPDATE_TIME:
+    const newTicket = Object.assign({}, state[id], {formattedWaitTime});
+    const updatedState = Object.assign({}, state, {
+      [id]: newTicket
+    });
+    return updatedState;
+  default:
+    return state;
   }
-
 };
