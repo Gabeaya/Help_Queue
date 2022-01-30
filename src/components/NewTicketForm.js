f2df4788fe8466bc06df3173b785a8422a44323d
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-30 15:13:10 -0800

    Add the firestore to the newticketform.

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,19 +1,29 @@
 import React from "react";
-import {v4} from 'uuid';
 import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
-import Moment from 'moment';
+import { useFirestore } from 'react-redux-firebase';
 
 function NewTicketForm(props){
-  function handleNewTicketFormSubmission(event) {
+  const firestore = useFireStore();
+
+  function addTicketToFireStore(event) {
     event.preventDefault();
     //when the user adds a ticket the moment time stamp will be created and the wait time will be set to the elapsed time since the ticket was open
-    props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4(), timeOpen: new Moment(), formattedWaitTime: new Moment().fromNow(true)});
+    props.onNewTicketCreation();
+    return firestore.collection('tickets').add(
+      {
+        names: event.target.names.value,
+        location: event.target.location.value, 
+        issue: event.target.issue.value,
+        timeOpen: firestore.FieldValue.serverTimestamp()
+      }
+    );
+  
   }
   return (
     <React.Fragment>
       <ReusableForm 
-        formSubmissionHandler={handleNewTicketFormSubmission}
+        formSubmissionHandler={addTicketToFireStore}
         buttonText="Help!" />
     </React.Fragment>
   );
