8a699512b8bbe11bc4ba5e09fc338e6426a80013
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-30 09:48:27 -0800

    Add the time and wait for the add ticket props.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import * as c from '../actions/ActionTypes';
 export const deleteTicket = id => ({
   type: 'DELETE_TICKET',
   id
@@ -6,13 +7,15 @@ export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
 export const addTicket = (ticket) => {
-  const { names, location, issue, id} = ticket;
+  const { names, location, issue, id, timeOpen, formattedWaitTime } = ticket;
   return {
     type:'ADD_TICKET',
     names: names,
     location: location,
     issue: issue,
-    id: id
+    id: id,
+    timeOpen: timeOpen,
+    formattedWaitTime: formattedWaitTime
   }
 }
 export const updateTime = (id, formattedWaitTime) => ({
