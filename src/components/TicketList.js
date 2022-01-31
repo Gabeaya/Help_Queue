14ec46cc8808cf47d5483bd28380ef85b4703afe
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-30 16:36:02 -0800

    Add firestore functionality.

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,28 +1,45 @@
 import React from "react";
 import Ticket from "./Ticket";
 import PropTypes from "prop-types";
-
+import { useSelector } from 'react-redux'; //allows us to extract data from the redux store
+import { useFirestoreConnect, isLoaded } from 'react-redux-firebase';//alows for listening of changes to firestore withojut class component
 
 function TicketList(props){
-  return (
-    <React.Fragment>
-      <hr/>
-      {Object.values(props.ticketList).map((ticket) =>
-        <Ticket
-          whenTicketClicked = { props.onTicketSelection }
-          names={ticket.names}
-          location={ticket.location}
-          issue={ticket.issue}
-          formattedWaitTime={ticket.formattedWaitTime}
-          id={ticket.id}
-          key={ticket.id}/>
-      )}
-    </React.Fragment>
-  );
+
+  //specify the collection of tickets we want to listen to in the firestore
+  useFirestoreConnect([
+    {collection: 'tickets'}
+  ]);
+
+  const tickets = useSelector(state => state.firestore.ordered.tickets);
+
+  if (isLoaded(tickets)) {//is loaded allows us to check if a collection has been retrieved
+    return (
+      <React.Fragment>
+        <hr/>
+        {tickets.map((ticket) =>
+          <Ticket
+            whenTicketClicked = { props.onTicketSelection }
+            names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue}
+            formattedWaitTime={ticket.formattedWaitTime}
+            id={ticket.id}
+            key={ticket.id}/>
+        )}
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <h3>Loading...</h3>
+      </React.Fragment>
+    )
+  }
+  
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.object,
   onTicketSelection: PropTypes.func
 };
 export default TicketList;
